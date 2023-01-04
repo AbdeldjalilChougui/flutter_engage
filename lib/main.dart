1ef094a410357e602c67b02f0ef68bbdbbba2b9f
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2023-01-04 15:24:28 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_engage/widgets/my_button.dart';
+
+void main() {
+  runApp(const MyFlutterApp());
+}
+
+// void main() => runApp(const MyApp());
+
+class MyFlutterApp extends StatelessWidget {
+  const MyFlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Engage',
+      home: MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 100,),
+              Center(child: Image.asset("images/logo.png")),
+              const SizedBox(height: 50,),
+              const Text("Welcome!", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 5),
+              const Text(
+                'Get in touch with millions people in the world!',
+                style: TextStyle(fontSize: 12, color: Colors.grey),),
+              const SizedBox(height: 50),
+              const Text('Continue as'),
+              const SizedBox(height: 20,),
+              const MyButton(text: "User GDSC"),
+              const SizedBox(height: 50,),
+              const Text("Login to another account"),
+              const SizedBox(height: 20),
+              TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: "Email address or phone number",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.transparent, ),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  fillColor: Colors.grey.shade300,
+                  filled: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    Key? key,
+    this.text,
+  }) : super(key: key);
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: const Color(0xff01A6AB),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(CupertinoIcons.person, color: Colors.white),
+          Column(
+            children: [
+              Text(text!, style: const TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w700)),
+              const Text("Active 20min ago", style: TextStyle(color: Colors.white, fontSize: 10),),
+            ],
+          ),
+          const Icon(Icons.arrow_forward, color: Colors.white, size: 25),
+        ],
+      ),
+    );
+  }
+}
